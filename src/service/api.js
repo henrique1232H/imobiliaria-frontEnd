cd64b7c9aa579819e6cb4be920412f4c2d87fe7e
henrique1232H <henriqueau111@gmail.com>
2024-12-17 16:18:57 -0300

    ajust

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://imobiliaria-backend.onrender.com"
+    baseURL: "https://imobiliaria-backend-1.onrender.com"
 })
